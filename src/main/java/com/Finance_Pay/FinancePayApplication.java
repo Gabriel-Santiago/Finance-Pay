89d38095cda9f5e00c235ea69a33e7dc1d2b4b5f
Gabriel <gabrielsrmj@alu.ufc.br>
2024-05-08 14:31:44 -0300

    Base inicial do projeto

---
+++ src/main/java/com/Finance_Pay/FinancePayApplication.java
@@ -0,0 +1,13 @@
+package com.Finance_Pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancePayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Finance_Pay/FinancePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Finance_Pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
